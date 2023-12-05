5d2de9a8b4eb441aaaec9d131caa6098e6abf2e9
choideakook <shdrnrhd113@naver.com>
2023-12-05 15:15:31 +0900

    feat: lint 세팅

--- src/lib/handlers.js
+++ src/lib/handlers.js
@@ -4,4 +4,6 @@ exports.about = (req, res) => res.render('about')
 
 exports.notFound = (req, res) => res.render('404');
 
-exports.serverError = (ERR, req, res, NEXT) => res.render('500');
+/* eslint-disable no-unused-vars */
+exports.serverError = (ERR, req, res, next) => res.render('500');
+/* eslint-disable no-unused-vars */
