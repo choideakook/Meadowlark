aa117c40729a9e9b8f7341cab7ecbdd012d83e8c
choideakook <shdrnrhd113@naver.com>
2023-12-06 17:27:56 +0900

    Feat: post form 생성

--- src/app.js
+++ src/app.js
@@ -38,6 +38,10 @@ app.get('/about', handlers.about)
 app.get('/headers', handlers.header)
 app.get('/section', handlers.sectionTest)
 
+app.get('/newsletter-signup', handlers.newsletterSignup)
+app.post('/newsletter-signup/process', handlers.newsletterSignupProcess)
+app.get('/newsletter-signup/thank-you', handlers.newsletterSignupThankYou)
+
 app.use(handlers.notFound)
 app.use(handlers.serverError)
 
--- src/lib/handlers.js
+++ src/lib/handlers.js
@@ -9,6 +9,22 @@ exports.header = (req, res) => {
     res.send(headers.join('\n'))
 }
 
+exports.newsletterSignup = (req, res) => {
+    res.render('newsletter-signup', {csrf: 'token here'})
+}
+
+exports.newsletterSignupProcess = (req, res) => {
+    console.log('Form (from query) :', req.query.form)
+    console.log('CSRF (from hidden form field) :', req.body._csrf)
+    console.log('Name :', req.body.name)
+    console.log('Email :', req.body.email)
+    res.redirect(303, '/newsletter-signup/thank-you')
+}
+
+exports.newsletterSignupThankYou = (req, res) => {
+    res.render('newsletter-signup-thank-you')
+}
+
 exports.sectionTest = (req, res) => res.render('section-test')
 
 exports.notFound = (req, res) => res.render('404');
